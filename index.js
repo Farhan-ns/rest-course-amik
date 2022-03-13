5a1137663b4f99d1490bae57fc8cc87383f5ce1d
Farhanunnasih <farhanunnasih@gmail.com>
2022-03-13 20:29:51 +0700

    Add and use express-query-parser middleware

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const { queryParser } = require('express-query-parser')
 const coursesRouter = require('./router/courses')
 
 const app = express()
 const URL = 'localhost'
 const PORT = 3000
+
+app.use(queryParser({
+    parseNull: true,
+    parseUndefined: true,
+    parseBoolean: true,
+    parseNumber: true
+}))
 
 app.get('/', (req, res) => {
     res.send('Welcome')
