83b7c97a6017c4438c3a358c826ca6dbfd94aa4d
Farhanunnasih <farhanunnasih@gmail.com>
2022-03-13 20:37:08 +0700

    Enable user to filter from query
    This is done by having 2 handlers for /course endpoint, and if request include at least 1 query it will call the second handler to filter using the query and just call the first handler instead if no query is included

--- router/courses.js
+++ router/courses.js
@@ -3,27 +3,45 @@ const data = require('../transform')
 
 const router = express.Router()
 
-const detailedData = data.map(matkul => {
-    const isMandatory = !matkul.nama.includes('*') //Courses with "*" are non-mandatory
-    const semester = matkul.kode.charAt(4) //Semester is included in Courses code
-    return {
-        ...matkul,
-        wajib: isMandatory,
-        semester    
-    }
-})
+// const detailedData = data.map(matkul => {
+//     const isMandatory = !matkul.nama.includes('*') //Courses with "*" are non-mandatory
+//     const semester = matkul.kode.charAt(4) //Semester is included in 4th digit of Courses code
+//     delete matkul.wajib
+//     return {
+//         ...matkul,
+//         sks: parseInt(matkul.sks),    
+//         isMandatory,
+//         semester: parseInt(semester)
+//     }
+// })
 
-const getCourseBySemester = (req, res) => {
-    const { semester } = req.params
-    filteredData = detailedData.filter(course => course.semester === semester)
+const hasQuery = (obj) => {
+    const hasQuery = (Object.keys(obj).length >= 1) ? true : false;
+    return hasQuery;
+}
+
+const getAllCourses = (req, res, next) => {
+    if (hasQuery(req.query)) {
+        next()
+        return
+    }
+    res.json(data)
+}
+
+const getCourseByQuery = (req, res, next) => {
+    const { query } = req
+
+    let filteredData = data
+    for (var param in query) {
+        filteredData = filteredData.filter(c => c[param] === query[param])
+    }
+
     res.json(filteredData)
 }
 
 //Endpoints
-router.get('/', (req, res) => {
-    res.json(detailedData)
-})
+// router.get('/', [getCourses])
 
-router.get('/semester/:semester', getCourseBySemester)
+router.get('/', [getAllCourses, getCourseByQuery])
 
 module.exports = router
