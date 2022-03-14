fb8e700f3c52579e7147ad44ab34c2793a242396
Farhanunnasih <farhanunnasih@gmail.com>
2022-03-14 20:57:52 +0700

    enable express.json()
    To parse request body with JSON format

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import express from "express"
 import { queryParser } from "express-query-parser"
 import { router as coursesRouter} from './router/courses.js'
 
-
 const app = express()
 const URL = 'localhost'
 const PORT = 3000
 
-app.use(queryParser({
+app.use(
+    queryParser({
     parseNull: true,
     parseUndefined: true,
     parseBoolean: true,
     parseNumber: true
-}))
+    }
+))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Welcome')
